7855d4c647a0a768508d5a98ac21fce4fd7e7275
C4mz0r <aut0x@hotmail.com>
2016-07-18 18:47:40 -0600

    Ability to read env variables

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require 'yaml'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +24,17 @@ module OdinFlickrStream
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Add the ability to read from the flickr.yml file
+    config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'flickr.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
+
+
+   
